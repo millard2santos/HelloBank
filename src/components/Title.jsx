0f6621b48019798608f0afae7509fe2983e8d97f
millard2santos <millard2santos@gmail.com>
2023-03-16 20:31:12 +0100

    main branch setup

---
+++ src/components/Title.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Title = () => {
+  return (
+    <div>
+        <span className='text-3xl leading-9 text-helloColor'>Hello</span><span className='text-3xl leading-9 text-white'>Bank</span>
+    </div>
+  )
+}
+
+export default Title
---
+++ tailwind.config.cjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors : {
+        'green100' : '#25544E',
+        'green80' : '#669A94',
+        'green60' : '#83ACA7',
+        'green40' : '#A3D9D4',
+        'green20' : '#D4E6E4E',
+        'helloColor' : '#EDC8C8'
+      }
+    },
+  },
+  plugins: [],
+}
